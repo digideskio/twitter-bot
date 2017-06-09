6a769b062747f9858c91764e1f2bb09eaf7dd4d6
toreini <toreini@gmail.com>
2017-06-10 00:34:57 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+********************************
+This file includes the logic of my twitter Bot
+
+Author: Ehsan Toreini
+Organization: Newcastle University
+
+********************************
+*/
+
+var Twitter = require('twitter');
+var config = require('./config.js');
+
+var T = new Twitter(config);
+
---
+++ config.js
@@ -0,0 +1,16 @@
+/*
+********************************
+This file includes the configurations for my twitter Bot
+
+Author: Ehsan Toreini
+Organization: Newcastle University
+
+********************************
+*/
+
+module.exports = {
+  consumer_key: '4y6rdxlwDGgsHKKu3QVkXuKnT',
+  consumer_secret: 'bdl32M3ImCy05sGDslThUoVWl2ph4o5FrR1zinOG4v6F4tKg6s',
+  access_token_key: '1360236296-IuWzuZhB9KUtzqbJqEOEbP6JtKovseYm7rP7hz5',
+  access_token_secret: '383MTpEkNiOgqOzcsgYTtNBl8ZDqcqOsE6gwLvZ1cchkv'
+}
